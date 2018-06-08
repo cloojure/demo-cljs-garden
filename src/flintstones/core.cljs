e4c1fbbe688d5c42cb400f180f78bcdf9ea2a3e4
Alan Thompson <cloojure@gmail.com>
2018-06-07 17:32:35 -0700

    Demo of CSS for Tim Christiansen

--- src/flintstones/core.cljs
+++ src/flintstones/core.cljs
@@ -1,6 +1,7 @@
 (ns flintstones.core
   (:require
     [garden.core :as garden]
+    [garden.stylesheet :as gstyle]
     [goog.style :as style]
     [oops.core :as oops]
     [reagent.core :as r]
@@ -24,20 +25,25 @@ Go ahead and edit it and see reloading in action. Again, or not.")
   [:p.someClass "I am a CSS hamster!"])
 
 ;-----------------------------------------------------------------------------
-(defn install-css [css-def]
+(defn install-css
+  "Compile a hiccup-format CSS spec and install in the browser"
+  [css-def]
   (let [css-str (garden/css css-def)]
     (style/installStyles css-str)
     css-str))
+;-----------------------------------------------------------------------------
 
 (println "Installed via Garden:  \n"
-  (install-css [:.doit-class {:background-color :#337ab7 ; or "#337ab7"
-                         :color            :white
-                         :text-align       :center
-                         :display          :inline-block
-                         :font-size        :16px
-                         :padding          "10px 15px 10px 15px" ; or [[:10px :15px :10px :15px]]
-                         :border           "1px solid"
-                         :border-radius    :5px}]))
+  (install-css [".doit-class"           ; or :.doit-class
+                {:background-color :#337ab7 ; or "#337ab7"
+                 :color            :white
+                 :text-align       "center" ; or :center
+                 :display          :inline-block
+                 :font-size        :16px
+                 :padding          "10px 15px 10px 15px" ; or [[:10px :15px :10px :15px]]
+                 :border           "1px solid"
+                 :border-radius    :5px}]))
+
 (def btn-doit-counter (r/atom 0))
 (defn doit-btn []
   [:button.doit-class
@@ -48,14 +54,50 @@ Go ahead and edit it and see reloading in action. Again, or not.")
 
 ;-----------------------------------------------------------------------------
 (println "Installed via Garden:  \n"
-         (install-css [:.para-1 {:background-color :lightblue
-                                 :color            :RebeccaPurple
-                                 :font-size        :16px }]))
+         (install-css [:div [:p {:background-color :lightblue
+                                 :color            :blue
+                                 :font-size        :16px}]]))
 
 (defn rand-para []
-  [:p.para-1
-"This is some test just show that we can type anything we desire, and use an internal style definition independent
-of any outside influences. It could go on and on and on and on and on and on and on and on and on...." ])
+  [:div
+   [:p {:style {:color (if (< 9 5) :red :purple)}}
+    "This is some test just show that we can type anything we desire, and use an internal style definition independent
+    of any outside influences. It could go on and on and on and on and on and on and on and on and on...."]])
+
+;-----------------------------------------------------------------------------
+(println   "#1"
+  (install-css
+    [:html                              ; .slm-structure-html
+     {:font-size                :10px
+      :-webkit-text-size-adjust :100%}]))
+
+(println  "#2"
+  (install-css [:* {:padding :0px}]))
+
+(def all-in "100%")
+(println  "#3"
+  (install-css
+    [:body
+     {:width       all-in
+      :min-width   (if (< 2 5)
+                     :220px             ; <= this one wins
+                     :333px)
+      :font-size   :1.7rem
+      :font-family "ntanffy02"
+      "overflow-y" "scroll"
+      :overflow-x :hidden  }
+     ]))
+
+; suppress IE responsive
+(println "#4"
+         (install-css
+           (gstyle/at-media
+             {:all               true
+              :-ms-high-contrast :none}
+             [:body {:width     :auto!important
+                     :min-width :1280px!important
+                     :overflow  :auto!important}
+              ])))
 
 ;-----------------------------------------------------------------------------
 (defn demo-component []
@@ -65,18 +107,12 @@ of any outside influences. It could go on and on and on and on and on and on and
    [rand-para]
   ])
 
-(defonce reload-counter (r/atom 0))
-
 (defn run []
-  (r/render [demo-component] (js/document.getElementById "tgt-div")))
+  (r/render
+    [demo-component]
+    (js/document.getElementById "tgt-div")))
 
-(defn figwheel-reload []
-  ; optionally touch your app-state to force rerendering depending on your application
-  ; (swap! app-state update-in [:__figwheel_counter] inc)
-  (println "reload-counter =" (swap! reload-counter inc)))
-
-(when (zero? @reload-counter)
-  (println "Initial load")
-  (figwheel-reload))
 (run)
+
+
 
---
+++ src/flintstones/structure.cljs
@@ -0,0 +1,11 @@
+(ns flintstones.structure
+  (:require
+    [garden.core :as garden]
+    [goog.style :as style]
+    [reagent.core :as r]
+    [reagent.format :as fmt]
+  ))
+
+(enable-console-print!)
+(println "printed from src/flintstones/structure.cljs. ")
+
